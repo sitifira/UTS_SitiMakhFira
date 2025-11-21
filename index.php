e241385d2685ba9d13511aaa2edfe2407be4d530
fira <fira76493@gmail.com>
2025-11-21 19:13:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biodata Siswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index.css">
-</head>
-
-<body>
-    <?php
-    include_once("navbar.php");
-    ?>
-
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-8 m-auto">
-                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-                    <div class="card-header app-header">
-                            <b class="app-title">EMPLOYEE MANAGER</b>
-                        </div>
-                        <div class="card-body">
-                            <h2 class="welcome-text">Selamat Datang di Aplikasi Employee Manager</h2>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+// Redirect to karyawan index page
+header("Location: karyawan/index.php");
+exit();
+?>
--- navbar.php
+++ navbar.php
@@ -1,21 +1,19 @@
-<nav class="navbar navbar-expand-lg custom-navbar" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/UTS_SitiMakhFira/">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="#navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/UTS_SitiMakhFira/">Siswa</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
+<nav class="navbar navbar-expand-lg" style="background-color:#000C8A" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="karyawan/index.php">
+                <i class="fas fa-users"></i> Karyawan
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
         </div>
-    </div>
-</nav>
+    </nav>
